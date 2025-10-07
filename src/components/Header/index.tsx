eb83533d5353a1efbb7f0102eae1f386b139a6c4
Chiazokam Echeta <chiazokamecheta@gmail.com>
2025-10-07 22:18:03 +0100

    feat: complete Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,12 +3,13 @@ import Image from "next/image";
 import { Breadcrumb } from "./Breadcrumb";
 import { Button } from "../ui/button";
 import { useSidebarStore } from "@/store";
+import { Avatar } from "../Avatar";
 
 export const Header = () => {
   const { setIsSidebarOpen } = useSidebarStore();
 
   return (
-    <header className="border-default-transparent/10 flex h-16 w-full items-center border-b px-4 py-4 text-zinc-950 md:px-8">
+    <header className="border-default-transparent/10 flex h-16 w-full items-center justify-between border-b px-4 py-4 text-zinc-950 md:px-8">
       <Breadcrumb className="hidden md:block" />
 
       <div className="flex items-center gap-5 md:hidden">
@@ -27,6 +28,19 @@ export const Header = () => {
           <Image src="/icons/Logomark.svg" width={24} height={24} alt="Digenty logo" />
           <p className="text-sm font-medium text-zinc-950">Digenty</p>
         </div>
+      </div>
+
+      <div className="flex items-center gap-4">
+        <Button variant="ghost" className="hidden h-7 rounded-full border border-dashed px-2! py-0.5! md:flex">
+          <Image src="/icons/question-fill.svg" width={16} height={16} alt="Close button" />
+          <p className="text-sm font-medium">Help</p>
+        </Button>
+
+        <Button variant="ghost" className="p-0!">
+          <Image src="/icons/notification-2.svg" width={16} height={16} alt="Close button" />
+        </Button>
+
+        <Avatar username="John Doe" className="size-8" />
       </div>
     </header>
   );
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,3 +6,40 @@ export const MOBILE_VIEWPORT = 768;
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+const simpleHash = (text: string) => {
+  let hash = 0;
+  for (let i = 0; i < text.length; i++) {
+    const char = text.charCodeAt(i);
+    hash = (hash << 5) - hash + char;
+    hash |= 0; // Ensure the number is a 32-bit integer
+  }
+  return Math.abs(hash) % 21;
+};
+
+export const generateRandomColor = (text: string) => {
+  const tailwindColors = [
+    "bg-red-100",
+    "bg-blue-100",
+    "bg-green-100",
+    "bg-yellow-100",
+    "bg-purple-100",
+    "bg-pink-100",
+    "bg-indigo-100",
+    "bg-teal-100",
+    "bg-orange-100",
+    "bg-lime-100",
+    "bg-cyan-100",
+    "bg-sky-100",
+    "bg-emerald-100",
+    "bg-fuchsia-100",
+    "bg-rose-100",
+    "bg-violet-100",
+    "bg-amber-100",
+    "bg-gray-100",
+    "bg-slate-100",
+    "bg-stone-100",
+  ];
+
+  return tailwindColors[simpleHash(text)];
+};
