d803c6a4dea4e6d824816f4f0f59466b8f19b9d2
Chiazokam Echeta <chiazokamecheta@gmail.com>
2025-10-22 16:32:32 +0100

    feat: implement options drawer for mobile table

--- src/components/StudentAndParentRecord/MobileCard.tsx
+++ src/components/StudentAndParentRecord/MobileCard.tsx
@@ -1,9 +1,16 @@
 import { Student } from "@/components/DataTable/types";
-import { MoreHorizontalIcon } from "lucide-react";
+import { EyeIcon, MoreHorizontalIcon } from "lucide-react";
+import { useState } from "react";
 import { Avatar } from "../Avatar";
+import DeleteBin from "../Icons/DeleteBin";
+import Edit from "../Icons/Edit";
+import UserMinus from "../Icons/UserMinus";
+import { MobileDrawer } from "../MobileDrawer";
 import { Button } from "../ui/button";
 
 export const MobileCard = ({ student }: { student: Student }) => {
+  const [isOptionsOpen, setIsOptionsOpen] = useState(false);
+
   return (
     <div className="bg-bg-subtle border-border-default rounded-sm border text-sm font-medium">
       <div className="border-border-default flex h-9.5 items-center justify-between border-b px-3">
@@ -12,9 +19,33 @@ export const MobileCard = ({ student }: { student: Student }) => {
           <p className="text-text-default">{student.name}</p>
         </div>
 
-        <Button variant="ghost">
+        <Button variant="ghost" onClick={() => setIsOptionsOpen(true)}>
           <MoreHorizontalIcon className="text-icon-default size-5" />
         </Button>
+
+        {isOptionsOpen && (
+          <MobileDrawer open={isOptionsOpen} setIsOpen={setIsOptionsOpen} title="Actions">
+            <div className="flex flex-col gap-2 p-4">
+              <Button className="bg-bg-state-secondary border-border-darker text-text-default h-8 border text-sm font-medium">
+                <EyeIcon className="text-icon-default-muted size-4" />
+                <span>View Student Profile</span>
+              </Button>
+              <Button className="bg-bg-state-secondary border-border-darker text-text-default h-8 border text-sm font-medium">
+                <Edit fill="var(--color-icon-default-muted)" className="size-4" />
+                <span>Edit Student Profile</span>
+              </Button>
+
+              <Button className="bg-bg-state-secondary border-border-darker text-text-default h-8 border text-sm font-medium">
+                <UserMinus fill="var(--color-icon-default-muted)" className="size-4" />
+                <span>Withdraw Student</span>
+              </Button>
+              <Button className="bg-bg-state-secondary border-border-darker text-text-default h-8 border text-sm font-medium">
+                <DeleteBin fill="var(--color-icon-destructive)" className="size-4" />
+                <span className="text-icon-destructive">Delete Student Profile</span>
+              </Button>
+            </div>
+          </MobileDrawer>
+        )}
       </div>
 
       <div className="border-border-default flex h-9.5 items-center justify-between border-b px-3">
