a8395340a8e3af32a1347c82d571e4e3193d7e33
unknown <joshlokosu18@gmail.com>
2025-10-22 15:21:55 +0100

    Updated components and rename  icons

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -11,36 +11,36 @@ export const Dashboard = () => {
       <div className="grid w-full grid-cols-2 gap-3 p-4 md:grid-cols-3 lg:grid-cols-4">
         <OverviewCard
           title="Fees collected"
-          icon={<Image src="/icons/cash-fill.svg" width={24} height={24} alt="Icon" />}
+          icon={<Image src="/icons/cash-fill.svg" width={17} height={17} alt="Icon" />}
           value="₦200,280"
           color="var(--color-green-500)"
-          iconBg="var(--green-200)"
+          iconBg="var(--teal-100)"
           percentage="+58%"
         />
         <OverviewCard
           title="outstanding fees"
-          icon={<Image src="/icons/Vector.svg" width={24} height={24} alt="Icon" />}
+          icon={<Image src="/icons/alert-fill.svg" width={17} height={17} alt="Icon" />}
           value="₦50,000"
           color="var(--color-green-500)"
           percentage="+100%"
-          iconBg="var(--orange-200)"
+          iconBg="var(--yellow-100)"
         />
         <OverviewCard
           title="students"
-          icon={<Image src="/icons/Vector (1).svg" width={10} height={10} alt="Icon" style={{ textAlign: "center", display: "flex" }} />}
+          icon={<Image src="/icons/user-fill.svg" width={10} height={10} alt="Icon" style={{ textAlign: "center", display: "flex" }} />}
           value="₦50,000"
           color="var(--red-500)"
           percentage="-10%"
-          iconBg="var(--blue-200)"
+          iconBg="var(--sky-100)"
         />
 
         <OverviewCard
           title="Expenses"
-          icon={<Image src="/icons/Vector (2).svg" width={24} height={24} alt="Icon" />}
+          icon={<Image src="/icons/indeterminate-circle-fill.svg" width={17} height={17} alt="Icon" />}
           value="₦0"
           color=""
           percentage=""
-          iconBg="var(--orange-50)"
+          iconBg="var(--pink-100)"
         />
       </div>
       <div className=":grid-cols-[1fr] grid items-start gap-3 p-4 md:grid-cols-[1fr_430px]">
