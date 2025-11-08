b1fe969dd01617941a24c7bfd1fec6241b36e70f
Chiazokam Echeta <chiazokamecheta@gmail.com>
2025-11-08 15:40:46 +0100

    fix: fix issues on staging

--- src/app/(core)/attendance/page.tsx
+++ src/app/(core)/attendance/page.tsx
@@ -1,10 +1,17 @@
-import AttendanceManagement from "@/components/AttendanceManagement";
-import React from "react";
+import { AttendanceManagement } from "@/components/AttendanceManagement";
+import { Spinner } from "@/components/ui/spinner";
+import { Suspense } from "react";
 
 export default function page() {
   return (
-    <div>
+    <Suspense
+      fallback={
+        <div className="flex h-screen items-center justify-center">
+          <Spinner className="size-16" />
+        </div>
+      }
+    >
       <AttendanceManagement />
-    </div>
+    </Suspense>
   );
 }
