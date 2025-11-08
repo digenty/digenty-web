7151d3ac2579b1aa12c55056c3e69434d03780e1
Chiazokam Echeta <chiazokamecheta@gmail.com>
2025-11-08 09:29:49 +0100

    fix: use Suspense

--- src/app/(core)/student-and-parent-record/page.tsx
+++ src/app/(core)/student-and-parent-record/page.tsx
@@ -1,5 +1,17 @@
 import StudentAndParentRecord from "@/components/StudentAndParent";
+import { Spinner } from "@/components/ui/spinner";
+import { Suspense } from "react";
 
 export default function StudentAndParentRecordPage() {
-  return <StudentAndParentRecord />;
+  return (
+    <Suspense
+      fallback={
+        <div className="flex h-screen items-center justify-center">
+          <Spinner className="size-16" />
+        </div>
+      }
+    >
+      <StudentAndParentRecord />
+    </Suspense>
+  );
 }
