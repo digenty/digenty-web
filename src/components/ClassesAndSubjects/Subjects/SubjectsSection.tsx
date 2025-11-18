180df8bac9148fb6cb3b5111df27869ac0ad255f
Chiazokam Echeta <chiazokamecheta@gmail.com>
2025-11-18 18:56:11 +0100

    fix: fix design misalignments

--- src/components/ClassesAndSubjects/Subjects/SubjectsSection.tsx
+++ src/components/ClassesAndSubjects/Subjects/SubjectsSection.tsx
@@ -1,20 +1,24 @@
 import Subject, { ClassItem } from "./Subject";
-import SubjectdHeader from "./SubjectHeader";
+import SubjectHeader from "./SubjectHeader";
 
 const mathClasses: ClassItem[] = [
   { id: 0, grade: "JSS 1A", subjectStatus: "Not Started" },
-  { id: 1, grade: "JSS 1A", subjectStatus: "In Progress" },
-  { id: 2, grade: "JSS 1A", subjectStatus: "Submitted" },
-  { id: 3, grade: "JSS 1A", subjectStatus: "Request Edit Access" },
-  { id: 4, grade: "JSS 1A", subjectStatus: "Submitted" },
+  { id: 1, grade: "JSS 2A", subjectStatus: "In Progress" },
+  { id: 2, grade: "JSS 3A", subjectStatus: "Submitted" },
+  { id: 3, grade: "SS 1A", subjectStatus: "Request Edit Access" },
+  { id: 4, grade: "SS 2A", subjectStatus: "Submitted" },
+  { id: 5, grade: "SS 3A", subjectStatus: "Submitted" },
 ];
 
 export default function SubjectsSection() {
   return (
-    <div className="flex flex-col gap-6">
-      <SubjectdHeader />
-      <Subject title="Mathematics" classes={mathClasses} />
-      <Subject title="Mathematics" classes={mathClasses} />
+    <div className="space-y-4 pb-10">
+      <SubjectHeader />
+      <div className="flex flex-col gap-6">
+        <Subject title="Mathematics" classes={mathClasses} />
+        <Subject title="English" classes={mathClasses} />
+        <Subject title="Yoruba" classes={mathClasses} />
+      </div>
     </div>
   );
 }
