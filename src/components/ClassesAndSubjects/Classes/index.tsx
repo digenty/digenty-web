c4a4fb6141e2c5e2a74cda29d5ae969145d672c4
Joshua Onaolapo <joshlokosu18@gmail.com>
2026-02-21 11:39:18 +0100

    add empty state and loading

--- src/components/ClassesAndSubjects/Classes/index.tsx
+++ src/components/ClassesAndSubjects/Classes/index.tsx
@@ -13,10 +13,10 @@ export const MyClasses = () => {
       <div className="flex-start bg-bg-muted flex w-full flex-col gap-1.5 rounded-lg pt-1 pr-1 pb-2 pl-1 md:max-w-219 md:gap-3 md:pt-1 md:pr-3 md:pb-3 md:pl-3">
         <h2 className="text-text-default px-6 pt-1.5 text-base font-semibold md:px-5 md:pt-3">My Classes</h2>
         {isLoading ? (
-          <div className="flex flex-col gap-3">
-            <Skeleton className="bg-bg-input-soft h-12 w-full" />
-            <Skeleton className="bg-bg-input-soft h-12 w-full" />
-            <Skeleton className="bg-bg-input-soft h-12 w-full" />
+          <div className="flex flex-col gap-6">
+            {Array.from({ length: 3 }).map((_, i) => (
+              <Skeleton key={i} className="bg-bg-input-soft h-30 w-full md:max-w-219" />
+            ))}
           </div>
         ) : (
           <>
