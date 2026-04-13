ce9623e653c76177d538627c670200aff99268fe
Chiazokam Echeta <chiazokamecheta@gmail.com>
2026-04-13 10:11:56 +0100

    fix: urgent fix

--- src/schema/staff.ts
+++ src/schema/staff.ts
@@ -5,5 +5,4 @@ export const staffSchema = yup.object().shape({
   lastName: yup.string().trim().required("Last Name is required!"),
   email: yup.string().required("Email is required!"),
   phoneNumber: yup.string().required("Phone Number is required!"),
-  password: yup.string().required("Password is required!"),
 });
