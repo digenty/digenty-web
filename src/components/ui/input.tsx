80231b7ed007346c4b82ce00591d1e9f732611de
Chiazokam Echeta <chiazokamecheta@gmail.com>
2026-04-17 15:26:12 +0100

    fix

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -9,7 +9,7 @@ function Input({ className, type, placeholder = " ", ...props }: React.Component
       placeholder={placeholder}
       data-slot="input"
       className={cn(
-        "file:text-foreground placeholder:text-text-muted selection:bg-bg-primary selection:text-text-primary dark:bg-input/30 dark:[:not(:placeholder-shown)]:bg-bg-basic-gray-accent border-input h-9 w-full min-w-0 rounded-md border bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+        "file:text-foreground placeholder:text-text-muted selection:bg-bg-primary selection:text-text-primary text-text-default dark:bg-input/30 dark:[:not(:placeholder-shown)]:bg-bg-basic-gray-accent border-input h-9 w-full min-w-0 rounded-md border bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
         "focus-visible:border-ring focus-visible:ring-border-highlight focus-visible:ring-2 focus-visible:ring-offset-2",
         "aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
         className,
